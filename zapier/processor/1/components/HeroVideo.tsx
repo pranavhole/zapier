2e1e9a8d2d07484d93ba5f27694957f428f62d77
pranavhole <pranavhole050610@gmail.com>
2025-04-10 02:02:49 +0000

    k

---
+++ zapier/processor/1/components/HeroVideo.tsx
@@ -0,0 +1,6 @@
+export const HeroVideo =()=>{
+    return <div className="flex justify-center">
+
+        <video src="" className= " max-w-xl" controls={false} muted autoPlay /> 
+        </div>
+}
